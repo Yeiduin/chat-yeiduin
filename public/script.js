5d45982d8aba6c177046c4b286cffdab6ae49008
yeiduin romero m <yoelyei22@gmail.com>
2024-07-30 20:48:27 -0500

    vamoos

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io("https://chat-yeiduin.onrender.com/");
 
 const loginScreen = document.getElementById("login-screen");
 const chatScreen = document.getElementById("chat-screen");
