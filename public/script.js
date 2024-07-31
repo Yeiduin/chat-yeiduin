a4561fd28781191d9f4367dab3efc27c3f8afaad
yeiduin romero m <yoelyei22@gmail.com>
2024-07-30 21:23:00 -0500

    nuevo

--- public/script.js
+++ public/script.js
@@ -1,6 +1,4 @@
-const socket = io(
-  "https://chat-yeiduin.onrender.com/" && "https://chat-yeiduin.vercel.app/"
-);
+const socket = io("https://chat-yeiduin.vercel.app/");
 
 const loginScreen = document.getElementById("login-screen");
 const chatScreen = document.getElementById("chat-screen");
